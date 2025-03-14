b7b6fd622413c8492b2b623163ada259ccc752a0
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2025-03-14 22:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-//your JS code here. If required.
 function delayedHello() {
-return new promise(resolve =>{
-	setTimeout() >= {
-		resolve(Hellow, world!);
-}, 1000);
-	});
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve("Hello, world!");
+        }, 1000);
+    });
 }
 
-delayedHello.then(message =>
-	document.getElementById("output").textcontent= message);
+delayedHello().then(message => {
+    document.getElementById("output").textContent = message;
 });
